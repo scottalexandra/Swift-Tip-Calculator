9338f4fd8e45c1130b0a1cff2d489461995355be
Alex Robinson <alexandra.scott335@gmail.com>
2016-04-21 17:15:55 -0600

    adding ios project files

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var totalTextField : UITextField!
+    @IBOutlet var taxPctSlider : UISlider!
+    @IBOutlet var taxPctLabel : UILabel!
+    @IBOutlet var resultsTextView : UITextView!
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI() {
+        totalTextField.text = String(format: "%0.2f", tipCalc.total)
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        resultsTextView.text = ""
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refreshUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +35,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func calculateTapped(sender: AnyObject) {
+        tipCalc.total = Double((totalTextField.text! as NSString).doubleValue)
+        
+        let possibleTips = tipCalc.returnPossibleTips()
+        
+        var results = ""
+        let keys = Array(possibleTips.keys).sort()
+        for tipPct in keys {
+            let tipValue = possibleTips[tipPct]!
+            let prettyTipValue = String(format:"%2f", tipValue)
+            results += "\(tipPct)%: \(prettyTipValue)\n"
+        }
+        
+        resultsTextView.text = results
+    }
+    
+    @IBAction func taxPercentageChanged(sender: AnyObject) {
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
+    }
+    
+    @IBAction func viewTapped(sender: AnyObject) {
+        totalTextField.resignFirstResponder()
+    }
 }
 
