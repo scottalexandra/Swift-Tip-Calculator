0ba75e4f967f6eb4a7c6a2a93cc826af380bf65e
Alex Robinson <alexandra.scott335@gmail.com>
2016-04-26 11:28:38 -0600

    changed model to show the total with the tip included, use Tuples and delegates. Added a table view for the tip and total suggestions

--- TipCalculatorModel.swift
+++ TipCalculatorModel.swift
@@ -17,22 +17,23 @@ class TipCalculatorModel {
             return total/(taxPct + 1)
         }
     }
-
+    
     init(total:Double, taxPct:Double) {
         self.total = total
         self.taxPct = taxPct
     }
     
-    func calcTipWithTipPct(tipPct: Double) -> Double {
-        return subtotal * tipPct
+    func calcTipWithTipPct(tipPct: Double) -> (tipAmt:Double, total:Double) {
+        let tipAmt = subtotal * tipPct
+        let finalTotal = total + tipAmt
+        return (tipAmt, finalTotal)
     }
     
-    func returnPossibleTips() -> [Int:Double] {
+    func returnPossibleTips() -> [Int:(tipAmt:Double, total:Double)] {
         
         let possibleTipsInferred = [0.15, 0.18, 0.20]
-        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
         
-        var retval = [Int:Double]()
+        var retval = [Int:(tipAmt:Double, total:Double)]()
         for possibleTip in possibleTipsInferred {
             let intPct = Int(possibleTip*100)
             retval[intPct] = calcTipWithTipPct(possibleTip)
@@ -40,5 +41,36 @@ class TipCalculatorModel {
         
         return retval
     }
+    
+}
 
+import UIKit
+
+class TestDataSource: NSObject, UITableViewDataSource {
+    
+    let tipCalc = TipCalculatorModel(total:33.25, taxPct: 0.06)
+    var possibleTips = Dictionary<Int, (tipAmt:Double, total:Double)>()
+    var sortedKeys:[Int] = []
+    
+    override init() {
+        possibleTips = tipCalc.returnPossibleTips()
+        sortedKeys = Array(possibleTips.keys).sort()
+        super.init()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sortedKeys.count
+    }
+    
+    func tableView(tableView:UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: nil)
+        let tipPct = sortedKeys[indexPath.row]
+        let tipAmt = possibleTips[tipPct]!.tipAmt
+        let total = possibleTips[tipPct]!.total
+        
+        cell.textLabel?.text = "\(tipPct)%:"
+        cell.detailTextLabel?.text = String(format: "Tip: $%0.2f, Total: $%0.2f", tipAmt, total)
+        return cell
+    }
 }
